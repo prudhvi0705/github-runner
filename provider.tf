6227b86fbcde7c9a0fd035956096892b65e06ca6
prudhvi75 <m.vprudhvi7@gmail.com>
2026-01-30 17:03:01 +0530

    variables

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "remote-state-86s-dev"
+    bucket = "prudhvi-terraform-state-dev"
     key    = "github-runner"
     region = "us-east-1"
     use_lockfile = true
